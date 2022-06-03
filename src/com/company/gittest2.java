68b44cf6ebdabb5a4e1db423f21b193bd55438ad
jiangnuo2205 <jiangnuo2205@163.com>
2022-06-03 22:33:38 +0800

    Initial commit

---
+++ src/com/company/gittest2.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class gittest2 {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("gittest2测试成功");
+        System.out.println("只添加一行字输出，第三次测试，用sourcetree");
+        System.out.println("哈哈哈,第四次用来巩固复习");
+
+
+    }
+}
